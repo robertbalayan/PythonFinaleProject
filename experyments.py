e1636912187a4d4b9c2f37d3a04c3a874b73dffe
Gagik010101 <116186054+Gagik010101@users.noreply.github.com>
2023-05-24 17:44:40 +0400

    Add files via upload

---
+++ experyments.py
@@ -0,0 +1,57 @@
+from random import randint
+
+names_list = ["Ալեքսանդր", "Արամ", "Անահիտ", "Իրինա", "Նարինե", "Արմեն", "Հայկ", "Սիլվա", "Սանասար", "Բաղդասար"]
+picked_names_set = set()
+picked_ages_list = []
+while len(picked_names_set) < 5:
+    random_index = randint(0, len(names_list)-1)
+    picked_names_set.add(names_list[random_index])
+for j in range(5):
+    random_age = randint(20, 40)
+    picked_ages_list.append(random_age)
+name_age_dict = dict(zip(picked_names_set, picked_ages_list))
+print(name_age_dict)
+n = 0
+
+class Base:
+
+    def __init__(self, name1, name2, name3, name4, name5):
+        self.man1 = name1
+        self.man2 = name2
+        self.man3 = name3
+        self.man4 = name4
+        self.man5 = name5
+        self.list = [self.man1, self.man2, self.man3, self.man4, self.man5]
+        self.K = None
+class Himar(Base):
+    def patasxan(self):
+        print(f"{self.list[self.list.index(self.K) + 1]}\nԲարև {self.man1}!")
+class Realist(Himar):
+    def Barev(self):
+        for self.K in self.list:
+            print(f"{self.man1}\nՈղջույն {self.list[self.list.index(self.K) + 1]}")
+            if self.K == name3:
+                himar.patasxan()
+            print(f"{self.list[self.list.index(self.K) + 1]}\nԲարև {self.man1}")
+            if self.K == self.man4:
+                break
+
+
+# class Positiv(Base):
+
+for Name in name_age_dict:
+    if n == 0:
+        name1 = Name
+    elif n == 1:
+        name2 = Name
+    elif n == 2:
+        name3 = Name
+    elif n == 3:
+        name4 = Name
+    else:
+        name5 = Name
+    n += 1
+
+arajin = Realist(name1, name2, name3, name4, name5)
+arajin.Barev()
+himar = Himar(name1, name2, name3, name4, name5)
